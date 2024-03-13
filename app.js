0aa3a8ad9b895fcbd82d37392b8fe9215a51687c
cervantesjuand <juancervantes.1389@gmail.com>
2024-03-12 23:10:21 -0500

    cambios Juan

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import RouteTipoElemento from "./src/routes/TipoElemento.Router.js";
+import RouteCategoeria from "./src/routes/Categoria.controller.jdcc.js";
+import RouteEmpaque from "./src/routes/Empaque.Router.jdcc.js";
+import RouteMedida from "./src/routes/UnidadMedida.Router.jdcc.js";
+import RouteUbicacion from "./src/routes/Ubicacion.Router.jdcc.js"
+import RouteElemento from "./src/routes/Elemento.Router.jdcc.js";
 
 const app=express();
 
@@ -14,6 +20,13 @@ app.get('/',(req, res)=> {
     console.log(process.env);
     res.send('Pagina inicial');
 });
+
+app.use('/tipo', RouteTipoElemento);
+app.use('/categoria', RouteCategoeria);
+app.use('/empaque', RouteEmpaque);
+app.use('/medida', RouteMedida);
+app.use('/ubicacion', RouteUbicacion);
+app.use('/elemento', RouteElemento);
 
 //Servidor
 app.listen(3000,()=>{

83e0b46b0e92bc5cc5d7bd2e031bd6d8ef393835
johanperdomo2004 <johanperdomo109@gmail.com>
2024-03-13 08:16:21 -0500

    sube los camnios el gordo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import routeBodega from "./src/routes/Bodegas.Router.jeph.js";
 
 const app=express();
 
@@ -14,7 +15,7 @@ app.get('/',(req, res)=> {
     console.log(process.env);
     res.send('Pagina inicial');
 });
-
+app.use('/bodega', routeBodega);
 //Servidor
 app.listen(3000,()=>{
     console.log("El servidor se esta ejecutando en el puerto 3000");
